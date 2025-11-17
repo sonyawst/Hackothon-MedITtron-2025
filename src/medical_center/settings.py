dbc042ca571a5f5096e68047c51ff376dd8225ce
ASUS ZenBook <doktorovaa04@mail.ru>
2025-11-17 16:01:46 +0300

    settings

--- src/medical_center/settings.py
+++ src/medical_center/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y+n(ck^=($ol^z4+gx5r(9#ffq_ws(t&fcd)d2c)sf43=6l6_w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'medical_center.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases   
 
 DATABASES = {
     'default': {
@@ -111,6 +111,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tumor_model', 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
